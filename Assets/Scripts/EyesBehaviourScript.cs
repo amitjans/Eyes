a516a6c118d46397d1521929b63482ff78ad892a
Adrian Acosta Mitjans <adrian.mitjans@gmail.com>
2019-10-29 14:31:16 -0700

    Terminada la variante 1

--- Assets/Scripts/EyesBehaviourScript.cs
+++ Assets/Scripts/EyesBehaviourScript.cs
@@ -1,6 +1,10 @@
 ﻿using Assets.Scripts;
 using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
 using UnityEngine;
+using UnityEngine.Networking;
 using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 
@@ -13,32 +17,85 @@ public class EyesBehaviourScript : MonoBehaviour
     private MqttClient client;
     private Data message;
 
+    private bool first;
+    private bool server;
+    private string address;
+    private string clientId;
+
     // Start is called before the first frame update
+    [Obsolete]
     private void Start()
     {
         anim = GetComponent<Animator>();
         names = new[] { "Angry 0", "Bored 0", "Sad 0", "Happy 0", "Surprised 0" };
         state = true;
         message = new Data();
-
-        client = new MqttClient("127.0.0.1", 1883, false, null);
-
-        // register to message received
-        client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
-
-        string clientId = Guid.NewGuid().ToString();
-        client.Connect(clientId);
-
-        // subscribe to the topic "/home/temperature" with QoS 2
-        client.Subscribe(new string[] { "eve/eyes" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+        first = true;
+        server = false;
+        address = GetLocalIPAddress();
+        clientId = Guid.NewGuid().ToString();
     }
 
     private void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
     {
         var mqtt = System.Text.Encoding.UTF8.GetString(e.Message);
         //{"anim":"ini","bcolor":"blue","extra":false}
-        //Debug.Log("Received: " + mqtt);
         message = JsonUtility.FromJson<Data>(mqtt);
+    }
+
+    IEnumerator Fade()
+    {
+        //try
+        //{
+        //    client = new MqttClient("192.168.0.8", 1883, false, null);
+        //    // register to message received
+        //    client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+        //    client.Connect(clientId);
+        //    server = true;
+        //}
+        //catch (Exception e)
+        //{
+        //    Debug.Log(e.Message);
+        //    Debug.Log(address);
+        //    server = false;
+        //}
+
+        var ip = 2;
+        while (!server)
+        {
+            try
+            {
+                var temp = address.Split("."[0]);
+                var aux = temp[0] + "." + temp[1] + "." + temp[2] + "." + ip;
+                Debug.Log(aux);
+                client = new MqttClient(aux, 1883, false, null);
+                // register to message received
+                client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+                client.Connect(clientId);
+                client.Subscribe(new string[] { "eve/eyes" }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_MOST_ONCE });
+                message = new Data() { anim = "blink", bcolor = "", extra = false};
+                server = true;
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+                server = false;
+                if (ip < 255)
+                {
+                    ip++;
+                }
+                else
+                {
+                    server = true;
+                }
+            }
+            yield return new WaitForSeconds(.001f);
+        }
+
+        // subscribe to the topic "/home/temperature" with QoS 2
+        
+
+        yield return null;
     }
 
     // Update is called once per frame
@@ -69,6 +126,32 @@ public class EyesBehaviourScript : MonoBehaviour
                 anim.SetInteger("Option", message.getValueOfAnim());
             }
         }
+
+        if (first)
+        {
+            Debug.Log("first: " + first);
+            first = false;
+            StartCoroutine("Fade");
+        }
+        else if (server)
+        {
+            Debug.Log("server: " + server);
+            StopAllCoroutines();
+            server = false;
+        }
         anim.SetInteger("Option", message.getValueOfAnim());
+    }
+
+    public string GetLocalIPAddress()
+    {
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new System.Exception("No network adapters with an IPv4 address in the system!");
     }
 }
